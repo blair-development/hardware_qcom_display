48feae353e6e730ce0c039def91213eb2d81fa7f
Yashovardhan Bhatt <quic_ybhatt@quicinc.com>
2024-10-03 12:32:52 +0530

    HWC: Implement getdisplayconfigurations API
    Implements the new composer3-V3 API to support populating
    VRR configs with display modes.
    
    Adds composer3-V3 for android version(s) greater than 14.
    
    Change-Id: I6f6fa59a98d2a0b03708f8df3ef8a6cefe408d71

--- composer/AidlComposer.h
+++ composer/AidlComposer.h
@@ -17,7 +17,7 @@
 /*
  * Changes from Qualcomm Innovation Center are provided under the following license:
  *
- * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
@@ -38,6 +38,7 @@ namespace display {
 namespace composer3 {
 using aidl::android::hardware::graphics::composer3::BnComposer;
 using aidl::android::hardware::graphics::composer3::Capability;
+using aidl::android::hardware::graphics::composer3::DisplayConfiguration;
 using aidl::android::hardware::graphics::composer3::IComposerClient;
 
 using aidl::vendor::qti::hardware::display::config::DisplayConfigAIDL;
